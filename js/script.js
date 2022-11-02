639134a0812a5866e8636bd5391d69a7fd9ed759
UsefMohamed <usefmuhamedelgohre1@gmail.com>
2022-11-02 05:22:11 -0700

    tesla

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let menu_btn = document.getElementById("menu_btn");
+let aside = document.getElementById("aside");
+let closing_btn = document.getElementById("closing_btn");
+let body = document.getElementById("body");
+document.onclick = function (e) {
+    if (e.target.id == "aside" && e.target.id == "menu_btn") {
+        aside.classList.remove("effect");
+        menu_btn.classList.remove("effect");
+    }
+}
+menu_btn.onclick = function () {
+    aside.classList.toggle("effect");
+}
+closing_btn.onclick = function () {
+    aside.classList.remove("effect")
+}
+
+let prog_bar = document.getElementById("prog-bar");
+let hieght = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    prog_bar.style.width = `${(scrollTop / hieght) * 100}%`;
+    console.log(`${(scrollTop / hieght) * 100}%`);
+})
