df55a412cb7a4a94fb527c230a30de19bacaa005
Usef Mohamed <117280064+UsefMohamed@users.noreply.github.com>
2022-11-03 17:59:42 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let menu_btn = document.getElementById("menu_btn");
 let aside = document.getElementById("aside");
-
+let section = document.getElementsByTagName("section");
+let blur_effect = document.querySelectorAll("span");
+let button = document.querySelector("button");
 document.onclick = function (e) {
   if (e.target.id == "aside" && e.target.id == "menu_btn") {
     aside.classList.remove("effect");
@@ -8,6 +10,11 @@ document.onclick = function (e) {
   }
 };
 menu_btn.onclick = function () {
+  aside.classList.add("effect");
+  aside.classList.add("opacity");
+  menu_btn.classList.toggle("aside-btn");
+};
+button.onclick = function () {
   aside.classList.toggle("effect");
   aside.classList.toggle("opacity");
 };
