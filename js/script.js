5e414fc2e2cb91e1619119e49cd68cb064a864ea
Usef Mohamed <117280064+UsefMohamed@users.noreply.github.com>
2022-11-04 08:37:19 +0200

    update project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let menu_btn = document.getElementById("menu_btn");
 let aside = document.getElementById("aside");
 let section = document.getElementsByTagName("section");
-let blur_effect = document.querySelectorAll("span");
 let button = document.querySelector("button");
 document.onclick = function (e) {
   if (e.target.id == "aside" && e.target.id == "menu_btn") {
@@ -13,10 +12,12 @@ menu_btn.onclick = function () {
   aside.classList.add("effect");
   aside.classList.add("opacity");
   menu_btn.classList.toggle("aside-btn");
+  document.getElementById("blur_ele").classList.add("show_blur");
 };
 button.onclick = function () {
   aside.classList.toggle("effect");
   aside.classList.toggle("opacity");
+  document.getElementById("blur_ele").classList.remove("show_blur");
 };
 let prog_bar = document.getElementById("prog-bar");
 let hieght =
